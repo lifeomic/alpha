c823e46d78de982407e6748e340fbd5458367cbe
Anthony Roach <anthony.roach@lifeomic.com>
2019-08-18 11:03:20 -0400

    improve TypeScript type declarations

--- src/Alpha.d.ts
+++ src/Alpha.d.ts
@@ -1,8 +1,22 @@
 declare module '@lifeomic/alpha' {
   import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
+  export interface RetryOptions {
+    attempts: number,
+    factor: number,
+    maxTimeout: 10000,
+    retryCondition: Function
+  }
+
+  export interface AlphaOptions extends AxiosRequestConfig {
+    return: RetryOptions,
+    lambda: Function
+  }
+
+  export type AlphaClient = AxiosInstance;
+
   interface AlphaConstructor {
-    new (config: AxiosRequestConfig): AxiosInstance;
+    new (target: string | Function | AlphaOptions, options: AlphaOptions): AlphaClient;
   }
 
   const Alpha: AlphaConstructor;
