299459c9d8f7e481e4427e24a679046a24beb639
James Beavers <james.beavers@lifeomic.com>
2020-11-11 18:25:40 -0500

    Fix types - move away from module declarations

--- src/Alpha.d.ts
+++ src/Alpha.d.ts
@@ -1,25 +1,23 @@
-declare module '@lifeomic/alpha' {
-  import { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-  interface RetryOptions {
-    attempts?: number,
-    factor?: number,
-    maxTimeout?: number,
-    retryCondition?: (err: Error) => boolean
-  }
-
-  export interface AlphaOptions extends AxiosRequestConfig {
-    retry?: RetryOptions,
-    lambda?: Function
-  }
-
-  export type AlphaInstance = AxiosInstance;
-
-  interface AlphaConstructor {
-    new (target: string | Function, options?: AlphaOptions): AlphaInstance;
-    new (options: AlphaOptions): AlphaInstance;
-  }
-
-  // default export
-  export = AlphaConstructor;
+interface RetryOptions {
+  attempts?: number,
+  factor?: number,
+  maxTimeout?: number,
+  retryCondition?: (err: Error) => boolean
 }
+
+export interface AlphaOptions extends AxiosRequestConfig {
+  retry?: RetryOptions,
+  lambda?: Function
+}
+
+export type AlphaInstance = AxiosInstance;
+
+interface AlphaConstructor {
+  new (target: string | Function, options?: AlphaOptions): AlphaInstance;
+  new (options: AlphaOptions): AlphaInstance;
+}
+
+declare const Alpha: AlphaConstructor;
+export default Alpha;
