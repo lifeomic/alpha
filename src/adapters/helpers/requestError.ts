ad78fbc87f2580df18729a7b04b2fda317c265ac
David Tanner <david.tanner@lifeomic.com>
2022-04-14 14:59:13 -0600

    Coverage tests

--- src/adapters/helpers/requestError.ts
+++ src/adapters/helpers/requestError.ts
@@ -3,10 +3,10 @@ import { HandlerRequest, AlphaOptions } from '../../types';
 import { AxiosError, AxiosResponse } from 'axios';
 
 export const isAxiosError = (err: any | AxiosError): err is AxiosError =>
-  (typeof err === 'object') && err.isAxiosError;
+  (typeof err === 'object') && !!err.isAxiosError;
 
 export const isAlphaRequestError = (err: any | RequestError): err is RequestError =>
-  (typeof err === 'object') && err.isAlphaRequestError;
+  (typeof err === 'object') && !!err.isAlphaRequestError;
 
 export class RequestError extends Error implements Omit<AxiosError, 'response' | 'toJSON' | 'isAxiosError'> {
   public code?: string;
